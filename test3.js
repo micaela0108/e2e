841dfabee6e7bd32c2afddee1023a90e01032ccb
micaela0108 <122472918+micaela0108@users.noreply.github.com>
2023-01-12 19:18:12 +0100

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,2 +1,6 @@
-console.log("Test3")
-console.log("Test4")
+const sites = require("./sites.json") 
+const ACTIVE  = 1;
+const INACTIVE = 0;
+sites.forEach(site => {
+    if (site.active == INACTIVE) {   console.log(site.uri) }  
+});
